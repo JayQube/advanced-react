0ff2741d41375a13e5a528cb820c2213e1557f60
JayQube <mrjayqube@yandex.ru>
2025-10-29 16:43:51 +0300

    lesson 28

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,5 +1,4 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { useTranslation } from 'react-i18next';
 // импортируем классы из файла со стилями
 import cls from './Navbar.module.scss';
