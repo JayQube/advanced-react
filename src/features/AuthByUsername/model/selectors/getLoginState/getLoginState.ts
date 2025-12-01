fcb61d557e8ef13115e0a95fbdba05cde55a03d7
JayQube <mrjayqube@yandex.ru>
2025-12-01 21:21:28 +0300

    lesson 36 no comments expect 'Error' string

--- src/features/AuthByUsername/model/selectors/getLoginState/getLoginState.ts
+++
@@ -1,6 +0,0 @@
-import { StateSchema } from 'app/providers/StoreProvider';
-
-// Достаем из стора все что связано с loginForm
-export const getLoginState = (
-  state: StateSchema,
-) => state?.loginForm;
